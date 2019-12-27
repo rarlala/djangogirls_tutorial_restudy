b0fc9192729fb33022c1f4a13908113b05535fe8
JEONGSUJI <wtw9611@gmail.com>
2019-12-27 15:52:46 +0900

    관리자 페이지에서 추가했던 post 내용을 post_list.html에 출력해주기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,35 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from blog.models import Post
 
 
 def post_list(request):
-    return HttpResponse('<html><body><h1>하이</h1></body></html>')
+    # [1번 방법]
+    # current_file_path = os.path.abspath(__file__)
+    # blog_file_path = os.path.dirname(current_file_path)
+    # root_file_path = os.path.dirname(blog_file_path)
+    #
+    # templates_file_path = os.path.join(root_file_path, 'templates')
+    # post_list_html_path = os.path.join(templates_file_path, 'post_list.html')
+    #
+    # f = open(post_list_html_path, 'rt')
+    # html = f.read()
+    # f.close()
+    #
+    # return HttpResponse(html)
+
+    # [2번 방법]
+    # content = loader.render_to_string('post_list.html', None, request)
+    # return HttpResponse(content)
+
+    posts = Post.objects.all()
+    context = {
+        'posts' : posts
+    }
+
+    # [3번 방법]
+    return render(request, 'post_list.html', context)
--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
